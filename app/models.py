d3039adae074a0b51e385aacf7e8d22fc74b5d24
Vishnu Ks <yo@vishnuks.com>
2019-02-12 01:55:17 +0530

    Deprecate Blog in favor of UserProfile.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
+class UserProfile(models.Model):
+    extra_data = models.TextField()
+    auth_user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    feed_url = models.CharField(max_length=200)
+    github_username = models.CharField(max_length=50)
+    github_id = models.IntegerField()
+    github_token = models.CharField(max_length=50, null=True)
+    is_activated = models.BooleanField(default=False)
+    full_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, null=True)
--- feed/models.py
+++ feed/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib import admin
 
-# Create your models here.
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    feed_url = models.CharField(max_length=200)
-    github_id = models.CharField(max_length=200)
+from app.models import UserProfile
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
     content = models.TextField()
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     updated_on = models.DateTimeField(null=True)
