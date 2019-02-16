658b9b16b0ec2564a2e60cebf47c32bc2d5b1d8e
Vishnu Ks <yo@vishnuks.com>
2019-02-16 21:04:37 +0530

    Use ajax for basic feed.

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,10 @@
 $(function () {
-    console.log("hello world")
+    var source = document.getElementById("feed-item-template").innerHTML;
+    var template = Handlebars.compile(source);
+    $.get("/api/posts", function (posts) {
+        for (index in posts) {
+            var html = template(posts[index]);
+            $("#home_feed").append(html);
+        }
+    });
 }); 
