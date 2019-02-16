199d7f604dbd5078ae37b2de9dfe3505bb8503c6
Vishnu Ks <yo@vishnuks.com>
2019-02-16 21:10:22 +0530

    implement infinite scroll.

--- static/js/home.js
+++ static/js/home.js
@@ -7,4 +7,15 @@ $(function () {
             $("#home_feed").append(html);
         }
     });
-}); 
+
+    $(window).scroll(function () { 
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+            $.get("/api/posts", function (posts) {
+                for (index in posts) {
+                    var html = template(posts[index]);
+                    $("#home_feed").append(html);
+                }
+            });
+        }
+     });
+});
