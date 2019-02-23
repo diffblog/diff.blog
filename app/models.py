b195609a3456250ff3e7a729f7cd38e69dd6bbdb
Vishnu Ks <yo@vishnuks.com>
2019-02-24 00:18:41 +0530

    models: Add topics.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.TextField()
+
 class UserProfile(models.Model):
     extra_data = models.TextField()
     auth = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name="profile")
@@ -11,6 +15,7 @@ class UserProfile(models.Model):
     github_token = models.CharField(max_length=50, null=True)
     is_activated = models.BooleanField(default=False)
     full_name = models.CharField(max_length=50)
+    topics = models.ManyToManyField(Topic)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
