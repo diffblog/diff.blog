4b528b13fa97662011bd047995169e4837b9ecc4
Vishnu Ks <yo@vishnuks.com>
2019-03-10 18:53:46 +0530

    handlebars: Use webpack for compiling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './static/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  }
+    mode: 'development',
+    entry: {
+        followers: './static/js/followers.js',
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            { test: /\.handlebars$/, loader: "handlebars-loader" }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
