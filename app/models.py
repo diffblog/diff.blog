cf3a5dc1077a607848ee4db0c8fb5e73b652a2ee
Vishnu Ks <yo@vishnuks.com>
2019-03-23 13:51:34 +0530

    Add reccomended users attribute to topic.

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
 class Topic(models.Model):
     display_name = models.CharField(max_length=30)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    recommended = models.ManyToManyField("UserProfile", related_name="featured_in")
 
 class UserProfile(models.Model):
     extra_data = models.TextField()
