3d6948daf01b5190a2653bca084dcf67230d524c
Vishnu Ks <yo@vishnuks.com>
2019-04-14 21:13:10 +0530

    webpack: Change webpack output directory to static.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,6 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'static')
     }
 };
