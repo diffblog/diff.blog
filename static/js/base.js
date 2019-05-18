ecb0916e3856f1dd7545f4215347b42a5e99c25c
Vishnu Ks <yo@vishnuks.com>
2019-05-18 14:04:53 +0530

    navbar: Make navbar end work in mobile.

--- static/js/base.js
+++ static/js/base.js
@@ -6,4 +6,9 @@ $(function() {
     $("#signup-prompt-modal .modal-close").on("click", function() {
         $("#signup-prompt-modal").removeClass("is-active");
     });
+
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
 });
