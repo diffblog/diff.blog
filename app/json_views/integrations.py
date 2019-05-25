8879c9475170620baddfb83eed91a0b037d7e290
Vishnu Ks <yo@vishnuks.com>
2019-05-25 18:34:31 +0530

    pocket: Redirect to integrations page if not authorized.

--- app/json_views/integrations.py
+++ app/json_views/integrations.py
@@ -10,6 +10,9 @@ import pocket
 
 def pocket_add(request):
     access_token = request.user.profile.pocket_api_key
+    if not access_token:
+        return JsonResponse({'status':'false','message':'authorize pocket'}, status=400)
+
     pocket_instance = pocket.Pocket(pocket_consumer_key, access_token)
     post = Post.objects.get(id=request.POST.get("post_id"))
     pocket_instance.add(url=post.link, title=post.title)
