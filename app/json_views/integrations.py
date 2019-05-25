f0a120caec58c6931057e1337a9402243e7ad404
Vishnu Ks <yo@vishnuks.com>
2019-05-25 17:56:17 +0530

    pocket: Implement saving to pocket functionality.

---
+++ app/json_views/integrations.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from dateutil.parser import parse as iso_date_parser
+from django.contrib.auth.decorators import login_required
+
+from app.models import CommentVote, Comment, Post
+from diffblog.secrets import pocket_consumer_key
+
+import pocket
+
+def pocket_add(request):
+    access_token = request.user.profile.pocket_api_key
+    pocket_instance = pocket.Pocket(pocket_consumer_key, access_token)
+    post = Post.objects.get(id=request.POST.get("post_id"))
+    pocket_instance.add(url=post.link, title=post.title)
+    return JsonResponse("Success", safe=False)
