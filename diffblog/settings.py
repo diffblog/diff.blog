cff2104c111f9c3e5aee5dd278d598daf6382f61
Vishnu KS <yo@vishnuks.com>
2019-12-01 12:51:52 +0530

    Use reccomended log formating from StackOverflow.

--- diffblog/settings.py
+++ diffblog/settings.py
@@ -229,21 +229,52 @@ LOGIN_URL = "/"
 if not DEVELOPMENT:
     LOGGING = {
         'version': 1,
-        'disable_existing_loggers': False,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt' : "%d/%b/%Y %H:%M:%S"
+            },
+        },
         'handlers': {
-            'file': {
-                'level': 'DEBUG',
-                'class': 'logging.FileHandler',
-                'filename': '/home/vishnu/diff.blog/var/log/django.log',
+            'null': {
+                'level':'DEBUG',
+                'class':'django.utils.log.NullHandler',
+            },
+            'logfile': {
+                'level':'DEBUG',
+                'class':'logging.handlers.RotatingFileHandler',
+                'filename': "/home/vishnu/diff.blog/var/log/django.log",
+                'maxBytes': 50000,
+                'backupCount': 2,
+                'formatter': 'standard',
+            },
+            'console':{
+                'level':'INFO',
+                'class':'logging.StreamHandler',
+                'formatter': 'standard'
             },
         },
         'loggers': {
             'django': {
-                'handlers': ['file'],
-                'level': 'DEBUG',
+                'handlers':['console'],
                 'propagate': True,
+                'level':'WARN',
             },
-        },
+            'django.db.backends': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+            'app': {
+                'handlers': ['console', 'logfile'],
+                'level': 'DEBUG',
+            },
+            'feed': {
+                'handlers': ['console', 'logfile'],
+                'level': 'DEBUG',
+            },
+        }
     }
 
 STATIC_URL = '/static/'
