2e67ac2f947c14e373b437b00c585e2360c8c4dd
Vishnu KS <yo@vishnuks.com>
2020-04-18 20:33:32 +0530

    management: Update score of posts only from last week.

--- app/management/commands/update_aggregate_votes_count_and_score_of_posts.py
+++ app/management/commands/update_aggregate_votes_count_and_score_of_posts.py
@@ -1,11 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
+from datetime import timedelta
+from django.utils import timezone
 
 from app.models import Post
 
 class Command(BaseCommand):
-    help = 'Update aggregate vote count and score of all posts'
+    help = 'Update aggregate vote count and score of all posts from last 1 week'
 
     def handle(self, *args, **options):
-        posts = Post.objects.all()
+        three_days_back = timezone.now() - timedelta(days=7)
+        posts = Post.objects.filter(updated_on__gte=three_days_back)
+
         for post in posts:
             post.update_aggregate_votes_count_and_score()
