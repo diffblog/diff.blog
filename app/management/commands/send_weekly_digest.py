88db1a9750b7f67a0df22c0423e68bd492f97a03
Vishnu KS <yo@vishnuks.com>
2020-09-20 23:59:34 +0530

    digest: Send emails using a single connection.

--- app/management/commands/send_weekly_digest.py
+++ app/management/commands/send_weekly_digest.py
@@ -28,6 +28,8 @@ class Command(BaseCommand):
 
         top_posts = get_posts_for_weekly_digest()
 
+        connection = mail.get_connection()   # Use default email connection
+
         to_emails = []
         for user in users:
             if user.auth is None or not user.auth.email:
@@ -39,10 +41,13 @@ class Command(BaseCommand):
         msg_plain = render_to_string('emails/digest.txt', {"top_posts": top_posts})
         msg_html = render_to_string('emails/compiled/digest.html', {"top_posts": top_posts})
 
+        messages = []
         for to_email in to_emails:
             msg = EmailMultiAlternatives(subject, msg_plain, from_email, [to_email])
             msg.attach_alternative(msg_html, "text/html")
             if options["test_run"]:
                 print(to_email)
             else:
-                msg.send()
+                messages.append(msg)
+
+        connection.send_messages(messages)
