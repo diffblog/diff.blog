707d4c80fc90322bb38f7a2e8a2ef233d0d086d9
Vishnu KS <yo@vishnuks.com>
2020-10-13 00:19:10 +0530

    sitemap: Make changefreq and lastmod dynamic for users and tags.

--- app/sitemap_classes.py
+++ app/sitemap_classes.py
@@ -3,6 +3,18 @@ from app.models import Post, UserProfile, Topic
 
 import datetime
 
+def get_changefreq_from_last_updated_time(updated_on):
+    today = datetime.datetime.now(datetime.timezone.utc)
+    seconds_since_last_updated = (today - updated_on).total_seconds()
+
+    if seconds_since_last_updated <= 60 * 60:
+        return "hourly"
+    if seconds_since_last_updated <= 60 * 60 * 24:
+        return "daily"
+    if seconds_since_last_updated <= 60 * 60 * 24 * 7:
+        return "weekly"
+    return "monthly"
+
 class TopicSitemap(Sitemap):
     priority = 0.5
 
@@ -10,30 +22,18 @@ class TopicSitemap(Sitemap):
         return Topic.objects.filter()
 
     def lastmod(self, topic):
-        last_post = Post.objects.filter(topics=topic).last()
+        last_post = Post.objects.filter(topics=topic).order_by('-updated_on').first()
         if last_post is not None:
             return last_post.updated_on
         return None
 
     def changefreq(self, topic):
-        last_post = Post.objects.filter(topics=topic).order_by('-updated_on').last()
+        last_post = Post.objects.filter(topics=topic).order_by('-updated_on').first()
 
         if last_post is None:
             return "monthly"
 
-        today = datetime.datetime.now(datetime.timezone.utc)
-        seconds_since_last_updated = (today - last_post.updated_on).total_seconds()
-
-        if seconds_since_last_updated <= 60 * 60:
-            return "hourly"
-
-        if seconds_since_last_updated <= 60 * 60 * 24:
-            return "daily"
-
-        if seconds_since_last_updated <= 60 * 60 * 24 * 7:
-            return "weekly"
-
-        return "monthly"
+        return get_changefreq_from_last_updated_time(last_post.updated_on)
 
 class PostSitemap(Sitemap):
     changefreq = "never"
@@ -46,11 +46,24 @@ class PostSitemap(Sitemap):
         return obj.updated_on
 
 class UserSitemap(Sitemap):
-    changefreq = "never"
     priority = 0.5
 
     def items(self):
         return UserProfile.objects.filter()
+
+    def lastmod(self, profile):
+        last_post = Post.objects.filter(profile=profile).order_by('-updated_on').first()
+        if last_post is not None:
+            return last_post.updated_on
+        return None
+
+    def changefreq(self, profile):
+        last_post = Post.objects.filter(profile=profile).order_by('-updated_on').first()
+
+        if last_post is None:
+            return "monthly"
+
+        return get_changefreq_from_last_updated_time(last_post.updated_on)
 
 sitemaps = {
     "post": PostSitemap,
