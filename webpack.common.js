c228d6c315187f45070b9ac8f5a2cdac8b00340d
Vishnu KS <yo@vishnuks.com>
2020-10-31 13:41:09 +0530

    dependencies: Remove underscore.

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var webpack = require('webpack');
 
 var Handlebars = require('handlebars/runtime');
 
