2a41ac92d6ebc009ed57bb6964a2f3e4642e3db4
Vishnu KS <yo@vishnuks.com>
2021-03-07 18:32:51 +0530

    companies: Fetch only organizations.

--- app/json_views/companies.py
+++ app/json_views/companies.py
@@ -6,5 +6,5 @@ from .profile import serialize_users
 def companies_by_topic(request):
     topic_slug = request.GET.get("topic", None)
     user_ids = Post.objects.filter(topics__slug=topic_slug).values_list("profile", flat=True).distinct()
-    users = UserProfile.objects.filter(id__in=list(user_ids)).order_by('-followers_count')
+    users = UserProfile.objects.filter(id__in=list(user_ids), is_organization=True).order_by('-followers_count')
     return JsonResponse(serialize_users(request, users), safe=False)
