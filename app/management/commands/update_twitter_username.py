c0ed1cab8cccf1e7fa019b9020943d9f0c69a5b7
Vishnu KS <yo@vishnuks.com>
2021-05-09 21:32:49 +0530

    twitter: Update logic of updating user twitter handles.

--- app/management/commands/update_twitter_username.py
+++ app/management/commands/update_twitter_username.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from app.models import Topic, UserProfile, UserList, Post, Topic, Category
 from diffblog.secrets import diffblog_github_access_token
 import requests as r
+from django.db.models import Q
 
 diffblog_headers = {"Authorization": "token {}".format(diffblog_github_access_token)}
 
@@ -29,7 +30,19 @@ class Command(BaseCommand):
     help = ""
 
     def handle(self, *args, **options):
-        users = UserProfile.objects.filter(twitter_username=None)
-
-        for user in users:
+        for user in UserProfile.objects.filter(twitter_username=None).filter(
+            is_activated=True
+        ):
+            get_user_profile_details(user)
+        for user in (
+            UserProfile.objects.filter(twitter_username=None)
+            .exclude(blog_url="")
+            .exclude(blog_url=None)
+        ):
+            get_user_profile_details(user)
+        for user in (
+            UserProfile.objects.filter(twitter_username=None)
+            .exclude(feed_url="")
+            .exclude(feed_url=None)
+        ):
             get_user_profile_details(user)
