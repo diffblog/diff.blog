bec6e68db41cff13f64e48f1075da3c94952286b
Vishnu KS <yo@vishnuks.com>
2023-09-03 17:43:09 +0100

    Use get_user_profile in star.py
    Related ticket https://github.com/diffblog/diff.blog/issues/8

--- app/management/commands/update_curated_lists.py
+++ app/management/commands/update_curated_lists.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from app.models import Topic, UserProfile, UserList
+from app.models import Topic, UserProfile, UserList, get_user_profile
 
 
 class Command(BaseCommand):
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     def update_top_tech_company_engineering_blogs(self):
         topic = Topic.objects.get(display_name="Engineering Team Blogs")
         users = topic.recommended.all()
-        hackerkid = UserProfile.objects.get(github_username="hackerkid")
+        hackerkid = get_user_profile("hackerkid")
         user_list, _ = UserList.objects.get_or_create(
             name="Top Engineering Blogs of Tech Companies", created_by=hackerkid
         )
